8366c4bda556d5cb08af29e8069574fd7c16b455
matthieu.ahkoon <matthieu.ahkoon@me.com>
2024-01-23 14:49:31 +0000

    Move file to new directory

---
+++ data/twelve_publishers.db/load.sql
@@ -0,0 +1,1 @@
+COPY twelve.revenue_song_stg FROM '/content/twelve/data/twelve_publishers.db/twelve_revenue_song_stg.csv' (FORMAT 'csv', quote '"', delimiter ',', header 1);
---
+++ data/twelve_publishers.db/schema.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA twelve;
+
+
+
+CREATE TABLE twelve.revenue_song_stg(author_id BIGINT, song_id BIGINT, "year" BIGINT, format VARCHAR, revenue BIGINT, birthdate DATE, sign_date DATE, membership_duration_in_days BIGINT, age INTEGER, age_group VARCHAR, tenure_group VARCHAR);
+
+
+
+
